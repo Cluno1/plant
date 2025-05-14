0cabfb7fda1c3d8591528beb8731f83c2f72a413
zld <17875477802@163.com>
2025-05-14 18:22:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Author: zld 17875477802@163.com
+ * @Date: 2025-05-14 18:17:46
+ * @LastEditors: zld 17875477802@163.com
+ * @LastEditTime: 2025-05-14 18:21:46
+ * @FilePath: \plant\src\App.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2025 by ${git_name_email}, All Rights Reserved. 
+-->
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    
+  </div>
+    
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
