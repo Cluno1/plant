90b60e90329fa66bd3a353a6f26aaa6addde64f9
zld <17875477802@163.com>
2025-06-01 03:55:32 +0800

    nn

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
     outDir: "docs",
     assetsDir: "assets"  // 添加这一行
   },
-  base: process.env.NODE_ENV === 'production' ? './' : '/',
+  // 修改 base 配置为你的仓库名
+  base: process.env.NODE_ENV === 'production' 
+    ? '/plant/'  // 替换为你的实际仓库名
+    : '/',
   publicDir: 'public'
 })
