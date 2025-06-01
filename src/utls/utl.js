e2aa41d97625bbb668172333e94b93d254add00a
zld <17875477802@163.com>
2025-06-01 08:57:09 +0800

    nm

--- src/utls/utl.js
+++ src/utls/utl.js
@@ -2,7 +2,7 @@
  * @Author: zld 17875477802@163.com
  * @Date: 2025-05-26 22:27:44
  * @LastEditors: zld 17875477802@163.com
- * @LastEditTime: 2025-06-01 01:17:51
+ * @LastEditTime: 2025-06-01 08:56:29
  * @FilePath: \plant\src\utls\utl.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -25,7 +25,7 @@ export function judgePeriod(nowData, type, plantKind, plantPeriod) {
         return {
             shiny: result.shiny,
             pest: result.pest,
-            plantUrl: `/plants/${plantKind}/${result.plantPeriod}.jpg`,
+            plantUrl: `plants/${plantKind}/${result.plantPeriod}.jpg`,
         };
     }
     let plantContent;
@@ -138,7 +138,7 @@ export function judgePeriod(nowData, type, plantKind, plantPeriod) {
     return {
         shiny: result.shiny,
         pest: result.pest,
-        plantUrl: `/plants/${plantKind}/${result.plantPeriod}.jpg`,
+        plantUrl: `plants/${plantKind}/${result.plantPeriod}.jpg`,
     };
 }
 /**
--- src/utls/utl.ts
+++ src/utls/utl.ts
@@ -2,7 +2,7 @@
  * @Author: zld 17875477802@163.com
  * @Date: 2025-05-26 22:27:44
  * @LastEditors: zld 17875477802@163.com
- * @LastEditTime: 2025-06-01 01:17:51
+ * @LastEditTime: 2025-06-01 08:56:29
  * @FilePath: \plant\src\utls\utl.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -64,7 +64,7 @@ export function judgePeriod(
    return {
     shiny: result.shiny,
     pest: result.pest,
-    plantUrl: `/plants/${plantKind}/${result.plantPeriod}.jpg`,
+    plantUrl: `plants/${plantKind}/${result.plantPeriod}.jpg`,
   };
   }
 
@@ -181,7 +181,7 @@ export function judgePeriod(
   return {
     shiny: result.shiny,
     pest: result.pest,
-    plantUrl: `/plants/${plantKind}/${result.plantPeriod}.jpg`,
+    plantUrl: `plants/${plantKind}/${result.plantPeriod}.jpg`,
   };
 }
 /**
